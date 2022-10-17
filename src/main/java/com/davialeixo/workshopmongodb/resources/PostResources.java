7465d6d7636e8b8c29487739216bd76f5d8ad84b
Davi Aleixo <Davialeixo_nogueira@outlook.com>
2022-10-17 15:16:14 -0300

    Obtendo um post por id

---
+++ src/main/java/com/davialeixo/workshopmongodb/resources/PostResources.java
@@ -0,0 +1,27 @@
+package com.davialeixo.workshopmongodb.resources;
+
+import com.davialeixo.workshopmongodb.domain.Post;
+import com.davialeixo.workshopmongodb.domain.User;
+import com.davialeixo.workshopmongodb.dto.UserDTO;
+import com.davialeixo.workshopmongodb.services.PostService;
+import com.davialeixo.workshopmongodb.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/posts")
+@RequiredArgsConstructor
+public class PostResources {
+    private final PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/davialeixo/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.davialeixo.workshopmongodb.services;
+import com.davialeixo.workshopmongodb.domain.Post;
+import com.davialeixo.workshopmongodb.domain.User;
+import com.davialeixo.workshopmongodb.repositories.PostRepository;
+import com.davialeixo.workshopmongodb.services.exception.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() ->
+                        new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+
+}
+
