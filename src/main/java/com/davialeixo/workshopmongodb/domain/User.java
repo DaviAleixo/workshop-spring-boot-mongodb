9051971c755babe1e309568bdda7d520d71c3850
Davi Aleixo <Davialeixo_nogueira@outlook.com>
2022-10-17 14:56:08 -0300

    Referenciando os post do usuario

--- src/main/java/com/davialeixo/workshopmongodb/domain/User.java
+++ src/main/java/com/davialeixo/workshopmongodb/domain/User.java
@@ -2,14 +2,14 @@ package com.davialeixo.workshopmongodb.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+
 @EqualsAndHashCode
 @Document
 public class User implements Serializable {
@@ -19,4 +19,45 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
